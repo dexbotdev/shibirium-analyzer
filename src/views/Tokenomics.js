d0f7385297df1116c210ff16c91f888a2a3281b8
DefiCody <ambergridpro@gmail.com>
2023-02-08 20:36:20 +0530

    update chart

--- src/views/Tokenomics.js
+++ src/views/Tokenomics.js
@@ -1,61 +1,24 @@
 /* eslint-disable */ 
+
+// ** MUI Imports
 import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
+import Link from '@mui/material/Link'
+import Avatar from '@mui/material/Avatar'
 import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
 import CardHeader from '@mui/material/CardHeader'
-import IconButton from '@mui/material/IconButton'
+import FormControl from '@mui/material/FormControl'
 import CardContent from '@mui/material/CardContent'
 import Grid from '@mui/material/Grid'
 import { styled, useTheme } from '@mui/material/styles'
+import useBgColor from 'src/@core/hooks/useBgColor'
 
 // ** Icons Imports
-import Poll from 'mdi-material-ui/Poll'
-import TrendingUp from 'mdi-material-ui/TrendingUp'
-import CurrencyUsd from 'mdi-material-ui/CurrencyUsd'
-import DotsVertical from 'mdi-material-ui/DotsVertical'
-
-// ** Custom Components Imports
-import CustomAvatar from 'src/@core/components/mui/avatar'
-import ReactApexcharts from 'src/@core/components/react-apexcharts'
-
 import TokenomicsChart from './TokenomicsChart';
-
-const data = [
-  {
-    title: '$48,568.20',
-    avatarColor: 'success',
-    subtitle: 'Total Profit',
-    icon: <TrendingUp sx={{ fontSize: '1.875rem', color: 'success.main' }} />
-  },
-  {
-    title: '$38,453.25',
-    avatarColor: 'primary',
-    subtitle: 'Total Income',
-    icon: <CurrencyUsd sx={{ fontSize: '1.875rem', color: 'primary.main' }} />
-  },
-  {
-    title: '$2,453.45',
-    avatarColor: 'secondary',
-    subtitle: 'Total Expense',
-    icon: <Poll sx={{ color: 'secondary.main' }} />
-  }
-]
-
-const series = [
-  {
-    name: 'Product A',
-    data: [29000, 22000, 25000, 18500, 29000, 20000, 34500]
-  },
-  {
-    name: 'Product B',
-    data: [0, 16000, 11000, 15500, 0, 12500, 9500]
-  },
-  {
-    name: 'Product C',
-    data: [0, 0, 0, 14000, 0, 11500, 12000]
-  }
-]
+ 
 
 // Styled Grid component
 const StyledGrid = styled(Grid)(({ theme }) => ({
@@ -71,138 +34,326 @@ const Tokenomics = () => {
   // ** Hook
   // ** Hook
   const theme = useTheme()
-
-  // Vars
-  const whiteColor = '#fff'
-  const yellowColor = '#ffe802'
-  const primaryColor = '#836af9'
-  const areaChartBlue = '#2c9aff'
-  const barChartYellow = '#ffcf5c'
-  const polarChartGrey = '#4f5d70'
-  const polarChartInfo = '#299aff'
-  const lineChartYellow = '#d4e157'
-  const polarChartGreen = '#28dac6'
-  const lineChartPrimary = '#9e69fd'
-  const lineChartWarning = '#ff9800'
-  const horizontalBarInfo = '#26c6da'
-  const polarChartWarning = '#ff8131'
-  const scatterChartGreen = '#28c76f'
-  const warningColorShade = '#ffbd1f'
-  const areaChartBlueLight = '#84d0ff'
-  const areaChartGreyLight = '#edf1f4'
-  const scatterChartWarning = '#ff9f43'
-  const labelColor = theme.palette.text.primary
-  const borderColor = theme.palette.action.focus
-  const gridLineColor = theme.palette.action.focus
-
-
-  const options = {
-    chart: {
-      stacked: true,
-      parentHeightOffset: 0,
-      toolbar: { show: false }
-    },
-    plotOptions: {
-      bar: {
-        borderRadius: 10,
-        columnWidth: '35%',
-        endingShape: 'rounded',
-        startingShape: 'rounded'
-      }
-    },
-    xaxis: {
-      axisTicks: { show: false },
-      axisBorder: { show: false },
-      categories: [2016, 2017, 2018, 2019, 2020, 2021, 2022]
-    },
-    yaxis: {
-      labels: {
-        formatter: value => (value > 999 ? `${(value / 1000).toFixed(0)}k` : `${value}`)
-      }
-    },
-    colors: [theme.palette.primary.main, theme.palette.success.main, theme.palette.secondary.main],
-    grid: {
-      strokeDashArray: 7,
-      padding: {
-        bottom: -10
-      }
-    },
-    legend: { show: false },
-    dataLabels: { enabled: false },
-    stroke: {
-      width: 6,
-      curve: 'smooth',
-      lineCap: 'round',
-      colors: [theme.palette.background.paper]
-    },
-    states: {
-      hover: {
-        filter: { type: 'none' }
-      },
-      active: {
-        filter: { type: 'none' }
-      }
-    },
-    responsive: [
-      {
-        breakpoint: theme.breakpoints.values.xl,
-        options: {
-          plotOptions: {
-            bar: {
-              columnWidth: '45%'
-            }
-          }
-        }
-      },
-      {
-        breakpoint: theme.breakpoints.values.lg,
-        options: {
-          plotOptions: {
-            bar: {
-              columnWidth: '50%'
-            }
-          }
-        }
-      },
-      {
-        breakpoint: theme.breakpoints.values.sm,
-        options: {
-          plotOptions: {
-            bar: {
-              columnWidth: '45%'
-            }
-          }
-        }
-      },
-      {
-        breakpoint: 430,
-        options: {
-          plotOptions: {
-            bar: {
-              columnWidth: '55%'
-            }
-          }
-        }
-      }
-    ]
+ 
+  // ** Hook
+  const colorClasses = useBgColor()
+  const donutColors = {
+    series1: '#fdd835',
+    series2: '#00d4bd',
+    series3: '#826bf8',
+    series4: '#32baff',
+    series5: '#ffa1a1'
   }
+  
+ 
 
   return (
     <Card>
-      <Grid container>
-        <StyledGrid item xs={12} sm={12}>
-          <CardContent sx={{ height: '100%', '& .apexcharts-xcrosshairs.apexcharts-active': { opacity: 0 } }}>
-              <TokenomicsChart 
-            yellow={yellowColor}
-            info={polarChartInfo}
-            grey={polarChartGrey}
-            primary={primaryColor}
-            green={polarChartGreen}
-            labelColor={labelColor}
-            warning={polarChartWarning}
-            />
+      <Grid container spacing={2}>
+        <Grid item xs={6} sm={6}>
+          <CardContent sx={{ height: '100%',  }}>
+              <TokenomicsChart  />
           </CardContent>
-        </StyledGrid> 
+        </Grid> 
+
+        <Grid item xs={5} sm={5} sx={{mt:15}}> 
+         <Box
+          sx={{ mb:5,
+            borderRadius: '4px',
+            color: 'text.primary',
+            padding: theme => theme.spacing(2.25, 2.75),
+            backgroundColor: colorClasses.primaryLight.backgroundColor
+          }}
+        >
+          <Box sx={{ width: '100%', display: 'flex', alignItems: 'center' }}>
+            <Avatar
+              variant='rounded'
+              sx={{
+                mr: 3,
+                width: '2.625rem',
+                height: '2.625rem',
+                backgroundColor: 'transparent',
+                border: theme => `1px solid ${theme.palette.primary.main}`
+              }}
+            >
+              <img width={23} height={20} alt='briefcase' src='/images/cards/briefcase.png' />
+            </Avatar>
+
+            <Box
+              sx={{
+                width: '100%',
+                display: 'flex',
+                flexWrap: 'wrap',
+                alignItems: 'center',
+                justifyContent: 'space-between'
+              }}
+            >
+              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <Typography sx={{ fontWeight: 500, fontSize: '0.875rem' }}>Token Total Supply</Typography> 
+              </Box>
+              <Box sx={{ display: 'flex' }}>
+                <Typography
+                  component='sup'
+                  variant='caption'
+                  sx={{ mt: 0.75, fontWeight: 500, color: 'text.primary', alignSelf: 'flex-start' }}
+                >
+                  $
+                </Typography>
+                <Typography variant='h5' sx={{ fontWeight: 600 }}>
+                  1,000,000,000
+                </Typography> 
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+        <Box
+          sx={{ mb:5,
+            borderRadius: '4px',
+            color: 'text.primary',
+            padding: theme => theme.spacing(2.25, 2.75),
+            backgroundColor: colorClasses.primaryLight.backgroundColor
+          }}
+        >
+          <Box sx={{ width: '100%', display: 'flex', alignItems: 'center' }}>
+            <Avatar
+              variant='filled'
+              sx={{
+                mr: 3,
+                backgroundColor: donutColors.series1,
+                width: '2.625rem',
+                height: '2.625rem',
+                //backgroundColor: 'transparent',
+                border: theme => `1px solid ${theme.palette.primary.main}`
+              }}
+            >
+              <img width={23} height={20} alt='briefcase' src='/images/cards/briefcase.png' />
+            </Avatar>
+
+            <Box
+              sx={{
+                width: '100%',
+                display: 'flex',
+                flexWrap: 'wrap',
+                alignItems: 'center',
+                justifyContent: 'space-between'
+              }}
+            >
+              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <Typography sx={{ fontWeight: 500, fontSize: '0.875rem' }}>Presale</Typography> 
+              </Box>
+              <Box sx={{ display: 'flex' }}>
+                <Typography variant='h5' sx={{ fontWeight: 600 }}>
+                  50
+                </Typography> 
+                <Typography
+                  component='sup'
+                  variant='caption'
+                  sx={{ mt: 0.75, fontWeight: 500, color: 'text.primary', alignSelf: 'flex-start' }}
+                >
+                  %
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+        <Box
+          sx={{ mb:5,
+            borderRadius: '4px',
+            color: 'text.primary',
+            padding: theme => theme.spacing(2.25, 2.75),
+            backgroundColor: colorClasses.primaryLight.backgroundColor
+          }}
+        >
+          <Box sx={{ width: '100%', display: 'flex', alignItems: 'center' }}>
+            <Avatar
+              variant='filled'
+              sx={{
+                mr: 3,
+                width: '2.625rem',
+                height: '2.625rem',
+                backgroundColor: donutColors.series2,
+                border: theme => `1px solid ${theme.palette.primary.main}`
+              }}
+            >
+              <img width={23} height={20} alt='briefcase' src='/images/cards/briefcase.png' />
+            </Avatar>
+
+            <Box
+              sx={{
+                width: '100%',
+                display: 'flex',
+                flexWrap: 'wrap',
+                alignItems: 'center',
+                justifyContent: 'space-between'
+              }}
+            >
+              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <Typography sx={{ fontWeight: 500, fontSize: '0.875rem' }}>Liquidity</Typography> 
+              </Box>
+              <Box sx={{ display: 'flex' }}>
+                <Typography variant='h5' sx={{ fontWeight: 600 }}>
+                 30
+                </Typography> 
+                <Typography
+                  component='sup'
+                  variant='caption'
+                  sx={{ mt: 0.75, fontWeight: 500, color: 'text.primary', alignSelf: 'flex-start' }}
+                >
+                  %
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+        <Box
+          sx={{ mb:5,
+            borderRadius: '4px',
+            color: 'text.primary',
+            padding: theme => theme.spacing(2.25, 2.75),
+            backgroundColor: colorClasses.primaryLight.backgroundColor
+          }}
+        >
+          <Box sx={{ width: '100%', display: 'flex', alignItems: 'center' }}>
+            <Avatar
+              variant='filled'
+              sx={{
+                mr: 3,
+                width: '2.625rem',
+                height: '2.625rem',
+                backgroundColor: donutColors.series3,
+                border: theme => `1px solid ${theme.palette.primary.main}`
+              }}
+            >
+              <img width={23} height={20} alt='briefcase' src='/images/cards/briefcase.png' />
+            </Avatar>
+
+            <Box
+              sx={{
+                width: '100%',
+                display: 'flex',
+                flexWrap: 'wrap',
+                alignItems: 'center',
+                justifyContent: 'space-between'
+              }}
+            >
+              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <Typography sx={{ fontWeight: 500, fontSize: '0.875rem' }}>Development</Typography> 
+              </Box>
+              <Box sx={{ display: 'flex' }}>
+                <Typography variant='h5' sx={{ fontWeight: 600 }}>
+                  10
+                </Typography> 
+                <Typography
+                  component='sup'
+                  variant='caption'
+                  sx={{ mt: 0.75, fontWeight: 500, color: 'text.primary', alignSelf: 'flex-start' }}
+                >
+                  %
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+        <Box
+          sx={{ mb:5,
+            borderRadius: '4px',
+            color: 'text.primary',
+            padding: theme => theme.spacing(2.25, 2.75),
+            backgroundColor: colorClasses.primaryLight.backgroundColor
+          }}
+        >
+          <Box sx={{ width: '100%', display: 'flex', alignItems: 'center' }}>
+            <Avatar
+              variant='filled'
+              sx={{
+                mr: 3,
+                width: '2.625rem',
+                height: '2.625rem',
+                backgroundColor: donutColors.series4,
+                border: theme => `1px solid ${theme.palette.primary.main}`
+              }}
+            >
+              <img width={23} height={20} alt='briefcase' src='/images/cards/briefcase.png' />
+            </Avatar>
+
+            <Box
+              sx={{
+                width: '100%',
+                display: 'flex',
+                flexWrap: 'wrap',
+                alignItems: 'center',
+                justifyContent: 'space-between'
+              }}
+            >
+              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <Typography sx={{ fontWeight: 500, fontSize: '0.875rem' }}>Team</Typography> 
+              </Box>
+              <Box sx={{ display: 'flex' }}>
+                <Typography variant='h5' sx={{ fontWeight: 600 }}>
+                  5
+                </Typography> 
+                <Typography
+                  component='sup'
+                  variant='caption'
+                  sx={{ mt: 0.75, fontWeight: 500, color: 'text.primary', alignSelf: 'flex-start' }}
+                >
+                  %
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+        </Box>    
+        <Box
+          sx={{ mb:5,
+            borderRadius: '4px',
+            color: 'text.primary',
+            padding: theme => theme.spacing(2.25, 2.75),
+            backgroundColor: colorClasses.primaryLight.backgroundColor
+          }}
+        >
+          <Box sx={{ width: '100%', display: 'flex', alignItems: 'center' }}>
+            <Avatar
+              variant='filled'
+              sx={{
+                mr: 3,
+                width: '2.625rem',
+                height: '2.625rem',
+                backgroundColor: donutColors.series5,
+                border: theme => `1px solid ${theme.palette.primary.main}`
+              }}
+            >
+              <img width={23} height={20} alt='briefcase' src='/images/cards/briefcase.png' />
+            </Avatar>
+
+            <Box
+              sx={{
+                width: '100%',
+                display: 'flex',
+                flexWrap: 'wrap',
+                alignItems: 'center',
+                justifyContent: 'space-between'
+              }}
+            >
+              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <Typography sx={{ fontWeight: 500, fontSize: '0.875rem' }}>Marketing</Typography> 
+              </Box>
+              <Box sx={{ display: 'flex' }}>
+                <Typography variant='h5' sx={{ fontWeight: 600 }}>
+                  5
+                </Typography> 
+                <Typography
+                  component='sup'
+                  variant='caption'
+                  sx={{ mt: 0.75, fontWeight: 500, color: 'text.primary', alignSelf: 'flex-start' }}
+                >
+                  %
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+        </Box>    
+  
+        </Grid> 
       </Grid>
     </Card>
   )
--- src/views/TokenomicsChart.js
+++ src/views/TokenomicsChart.js
@@ -1,110 +1,114 @@
-/* eslint-disable */ 
-import Box from '@mui/material/Box'
+// ** MUI Imports
 import Card from '@mui/material/Card'
-import Grid from '@mui/material/Grid'
-import Divider from '@mui/material/Divider'
-import { useTheme } from '@mui/material/styles'
 import CardHeader from '@mui/material/CardHeader'
-import IconButton from '@mui/material/IconButton'
-import Typography from '@mui/material/Typography'
 import CardContent from '@mui/material/CardContent'
 
-// ** Icons Imports
-import Circle from 'mdi-material-ui/Circle'
-import CurrencyUsd from 'mdi-material-ui/CurrencyUsd'
-import DotsVertical from 'mdi-material-ui/DotsVertical'
- 
-// ** Third Party Imports
-import { PolarArea } from 'react-chartjs-2'
-// ** Custom Components Imports
-import CustomAvatar from 'src/@core/components/mui/avatar' 
+// ** Component Import
+import ReactApexcharts from 'src/@core/components/react-apexcharts'
 
-// ** Util Import
-import { hexToRGBA } from 'src/@core/utils/hex-to-rgba'
+const donutColors = {
+  series1: '#fdd835',
+  series2: '#00d4bd',
+  series3: '#826bf8',
+  series4: '#32baff',
+  series5: '#ffa1a1'
+}
 
-const TokenomicsChart = (props) => {
-  // ** Hook
-  const theme = useTheme()
-
-  const { info, grey, green, yellow, primary, warning, labelColor } = props
-
+const TokenomicsChart = () => {
   const options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    animation: { duration: 500 },
-    layout: {
-      padding: {
-        top: -5,
-        bottom: -45
+    legend: {
+      show: false,
+      position: 'right',
+      fontSize: '1rem',
+      fontFamily: 'Montserrat',
+      color:'white',
+    },
+    stroke: { width: 0 },
+    labels: ['Presale', 'Liquidity', 'Development', 'Team','Marketing'],
+    colors: [donutColors.series1, donutColors.series2, donutColors.series3, donutColors.series4, donutColors.series5],
+    dataLabels: {
+      enabled: true,
+      formatter(val) {
+        return `${parseInt(val, 10)}%`
       }
     },
-    scales: {
-      r: {
-        grid: { display: false },
-        ticks: { display: false }
-      }
-    },
-    plugins: {
-      legend: {
-        position: 'right',
-        labels: {
-          padding: 25,
-          boxWidth: 9,
-          color: labelColor,
-          usePointStyle: true
+    plotOptions: {
+      pie: {
+        donut: {
+          labels: {
+            show: true,
+            name: {
+              fontSize: '2rem',
+              fontFamily: 'Montserrat'
+            },
+            value: {
+              fontSize: '1rem',
+              fontFamily: 'Montserrat',
+              color:'white',
+              formatter(val) {
+                return `${parseInt(val, 10)}`
+              }
+            },
+            total: {
+              show: false,
+              fontSize: '1.5rem',
+              label: 'Operational',
+              formatter() {
+                return '31%'
+              }
+            }
+          }
         }
       }
-    }
-  }
-
-  const data = {
-    labels: ['40% Presale', '30% Liquidity', '10% Development', '5% Team', '5% Marketing', '10% Buffer'],
-    datasets: [
+    },
+    responsive: [ 
       {
-        borderWidth: 0,
-        label: 'Population (millions)',
-        data: [19, 17.5, 15, 13.5, 11, 9],
-        backgroundColor: [primary, yellow, warning, info, green, green]
+        breakpoint: 576,
+        options: {
+          chart: {
+            height: 400
+          },
+          plotOptions: {
+            pie: {
+              donut: {
+                labels: {
+                  show: false,
+                  name: {
+                    fontSize: '1.5rem',
+                    color:'white',
+                    fontWeight:'bolder'
+                  },
+                  value: {
+                    fontSize: '1rem',color:'success', fontWeight:'bolder'
+                  },
+                  total: {
+                    fontSize: '1.5rem',color:'success', fontWeight:'bolder'
+                  }
+                }
+              }
+            }
+          }
+        }
       }
     ]
   }
+  const series = [50, 30, 10, 5,5]
 
   return (
     <Card>
       <CardHeader
-        title={<h2>Tokenomics</h2>}
-        titleTypographyProps={{
-          sx: { lineHeight: '0rem !important', letterSpacing: '0.15px !important' }
-        }}
-        action={
-          <IconButton size='small' aria-label='settings' className='card-more-options' sx={{ color: 'text.secondary' }}>
-            <DotsVertical />
-          </IconButton>
-        }
+        title='Tokenomics'
+        titleTypographyProps={{ variant: 'h6' }}
+        subheader='Spending on various categories'
+        subheaderTypographyProps={{ variant: 'caption', sx: { color: 'white' } }}
       />
       <CardContent
-        sx={{
-          '& .apexcharts-datalabel-label': {
-            lineHeight: '1.313rem',
-            letterSpacing: '0.25px',
-            fontSize: '1.2rem !important',
-            fontWeight:'bolder',
-            fill: `${theme.palette.text.secondary} !important`
-          },
-          '& .apexcharts-datalabel-value': {
-            letterSpacing: 0,
-            lineHeight: '2rem',
-            fontWeight: '500 !important'
-          }
+        sx={{ color: 'white',
+          '& .apexcharts-canvas .apexcharts-pie .apexcharts-datalabel-label, & .apexcharts-canvas .apexcharts-pie .apexcharts-datalabel-value':
+            { fontSize: '1.7rem',  }
         }}
       >
-        <Grid container sx={{ my: [0, 4, 1.625] }}>
-          <Grid item xs={6} sm={6} sx={{ mb: [3, 0] }}>
-          <PolarArea data={data} options={options} height={350} />
-
-          </Grid>
-          
-        </Grid>
+        <ReactApexcharts options={options} series={series} type='donut' height={400} />
       </CardContent>
     </Card>
   )
